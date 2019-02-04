eef3d55d8446d84facba432db5f2c824d71e53f0
Jack Reacher <energylab100@gmail.com>
2019-02-03 19:51:06 -0800

    GitKraken -Feb3

---
+++ test.py
@@ -0,0 +1,9 @@
+#import matplotlib
+import matplotlib
+from awesome_colors import kite_blue
+
+kite_smarts = [1, 10, 9, 1000] #etc
+
+matplotlib.pyplot.plot(kite_smarts, color=kite_blue())
+matplotlib.pyplot.savefig("out.png")
+
