f9e02dc728407fcb0effe7b310b0192b5ba830ec
ParzivalWins <exercisetech@gmail.com>
2020-05-24 17:22:58 -0700

    Did my work

---
+++ start.py
@@ -0,0 +1,11 @@
+def function RecursionError
+
+# function definition and declaration
+def calculate_sum(a,b):
+  sum = a+b
+  return sum
+
+# The below statement is called function call
+print(calculate_sum(2,3)) # 5
+//TODO @parzival 
+OK
